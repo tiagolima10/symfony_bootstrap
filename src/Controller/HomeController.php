31428aab59bd3fe0bd9f4af57076748307ecbe56
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-24 09:24:03 -0300

    Primeiro Commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function new(): Response 
+    {
+        return $this->render("home/home.html.twig", [
+            
+        ]);
+    }
+}
+
+?>
